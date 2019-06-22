a2fb179c85ea0ac2803e09d5ea6af18333c7c86c
Edwin Capel <edwinrc1026@gmail.com>
2019-06-23 00:50:09 +0800

    Updates

---
+++ precache-manifest.9eafec2230dc2788224929ad223f78df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a007b5e91f55d9122d56a872cb2fa3",
+    "url": "/nextagram-react-web/index.html"
+  },
+  {
+    "revision": "bae23cf8da8069893019",
+    "url": "/nextagram-react-web/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6b575624addb4a662cc8",
+    "url": "/nextagram-react-web/static/css/main.57861297.chunk.css"
+  },
+  {
+    "revision": "bae23cf8da8069893019",
+    "url": "/nextagram-react-web/static/js/2.ac4151a2.chunk.js"
+  },
+  {
+    "revision": "6b575624addb4a662cc8",
+    "url": "/nextagram-react-web/static/js/main.388f8266.chunk.js"
+  },
+  {
+    "revision": "66a86c62c4b70285d9e6",
+    "url": "/nextagram-react-web/static/js/runtime~main.22f3a763.js"
+  }
+]);
